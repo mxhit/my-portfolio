417c10251b1dd5a929960a5b7db86e5207ac8996
Mohit Dodhia <mhtdodhia@gmail.com>
2021-09-13 23:41:59 +0530

    Updated README

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
