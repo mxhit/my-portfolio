8513828bdb4c19b29c294dc2ac0b5f0f2246a659
Mohit Dodhia <mhtdodhia@gmail.com>
2021-09-20 10:17:15 +0530

    Replaced avatar. Changing theme elements

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
                     mb:16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-600">
                         Hi, I'm Mohit.
                     </h1>
                     <p className="mb-8 leading-relaxed">
@@ -18,8 +18,7 @@ export default function About() {
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
-                        src="./coding.svg">
-
+                        src="./img/my-avatar-compressed.png">
                     </img>
                 </div>
             </div>
