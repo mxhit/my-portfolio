42bd5286df8e0e94e1255ff89543c8daa8879fcc
Mohit Dodhia <mhtdodhia@gmail.com>
2021-10-16 11:07:06 +0530

    Contact form fix test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ export default function Contact() {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", name, email, message }),
+            body: encode({ "form-name": "contactForm", name, email, message }),
         })
             .then(() => alert("Message sent!"))
             .catch((error) => alert(error));
