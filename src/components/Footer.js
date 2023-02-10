77eea98db15a5d98e6ad86a961e24a0191f82e9d
Mohit Dodhia <mhtdodhia@gmail.com>
2023-02-10 17:33:46 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,18 @@
 import React from "react";
-import { FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaStackOverflow } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md"
 
 export default function Footer() {
-    return(
+    return (
         <footer className="bg-orange-accent">
             <div className="container mx-auto flex flex-row flex-nowrap gap-x-2 justify-center items-center py-4">
-                <a href="https://twitter.com/mxhit" target="_blank" rel="noopener noreferrer">
-                    <FaTwitter className="w-6 h-6 text-white" />
-                </a>
-                <a href="https://www.instagram.com/mxhit/" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram className="w-6 h-6 text-white" />
-                </a>
                 <a href="https://www.linkedin.com/in/mxhit/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin className="w-6 h-6 text-white" />
                 </a>
                 <a href="https://github.com/mxhit" target="_blank" rel="noopener noreferrer">
                     <FaGithub className="w-6 h-6 text-white" />
                 </a>
-                <a href="https://stackoverflow.com/users/6817140/mohit-dodhia" target="_blank" rel="noopener noreferrer">
-                    <FaStackOverflow className="w-6 h-6 text-white" />
-                </a>
-                <a href="mailto:m.dodhia@outlook.com" target="_blank" rel="noopener noreferrer">
+                <a href="mailto:mohit@dodhia.in" target="_blank" rel="noopener noreferrer">
                     <MdAlternateEmail className="w-6 h-6 text-white" />
                 </a>
             </div>
